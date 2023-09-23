2a2f0a10fad175c2332afb5f889477b40a6b170a
Henri Johansson <henri.mm.johansson@gmail.com>
2023-09-23 21:55:14 +0300

    Tailwind installed.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="height:600px; width:800px">
+  <div style="height: 600px; width: 800px">
     <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -12,7 +12,7 @@
 
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import {LMap, LTileLayer} from "@vue-leaflet/vue-leaflet";
 
 export default {
   components: {
