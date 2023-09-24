dc7b1421b846192a0200c63f7c9d8b3e8307c6a9
Henri Johansson <henri.mm.johansson@gmail.com>
2023-09-24 14:27:06 +0300

    Revert "unused import"
    This reverts commit e8a72fe4158be7444d5d893735323143dac4871d.
    
    merge conflict reverse

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import Map from "@/components/Map.vue";
 import CreateMarker from "@/components/CreateMarker.vue";
 export default defineComponent({
