89c4e8d56915cacd8c76dac490fb8d51cc15fb32
Henri Johansson <henri.mm.johansson@gmail.com>
2023-10-09 10:50:03 +0300

    Removed logs and fixed packages added graphql, graphql-scalars and type-graphql

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,8 @@
 <template>
     <div style="height: 75vh; width: 50vw;">
-        <l-map v-model="zoom" v-model:zoom="zoom" v-model:center="centerRef" @move="log('move')" @click=placeOnClick(centerRef)>
+        <l-map v-model="zoom" v-model:zoom="zoom" v-model:center="centerRef" @click=placeOnClick(centerRef)>
             <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
-            <l-marker :key="index" v-for="(marker, index) in MarkerStoreHooks.getMarkArray()" :lat-lng="marker.location.coordinates" draggable
-                @moveend="log('moveend')">
+            <l-marker :key="index" v-for="(marker, index) in MarkerStoreHooks.getMarkArray()" :lat-lng="marker.location.coordinates" draggable>
                 <l-popup>{{ marker.marker_name }}</l-popup>
             </l-marker>
         </l-map>
@@ -11,7 +10,6 @@
 </template>
 
 <script setup lang="ts">
-import { UUIDMark } from "@/interfaces/Mark";
 import { MarkerStoreHooks } from "@/store/MarkerStore";
 import {
     LMap,
@@ -25,10 +23,6 @@ import { defineEmits, ref } from "vue";
 const centerRef = ref<[number, number]>([47.41322, -1.219482])
 const zoom = 2;
 const emit = defineEmits(['reload'])
-
-const log = (a: string): void => {
-  console.log(a);
-};
 
 const placeOnClick = (coordinates: [number, number] | undefined): void => {
   const placedMarker = MarkerStoreHooks.getChosenMark()
